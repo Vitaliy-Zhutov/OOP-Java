0424ffbec7a746757cabec4a2ed2d414dc5d9e49
vitaly <vitaly.zutov@yandex.ru>
2023-06-10 22:43:40 +0700

    add oop seminar_2

---
+++ seminar_2/Actor.java
@@ -0,0 +1,33 @@
+package seminar_2;
+
+public abstract class Actor implements ActorBehavior {
+    protected String name;
+    protected boolean isMakeOrder;
+    protected boolean isTakeOrder;
+
+    public Actor(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setMakeOrder() {
+        System.out.println(name + " сделал заказ.");
+        isMakeOrder = true;
+    }
+
+    @Override
+    public void setTakeOrder() {
+        System.out.println(name + " забрал заказ.");
+        isTakeOrder = true;
+    }
+
+    @Override
+    public boolean isMakeOrder() {
+        return isMakeOrder;
+    }
+
+    @Override
+    public boolean isTakeOrder() {
+        return isTakeOrder;
+    }
+}
---
+++ seminar_2/ActorBehavior.java
@@ -0,0 +1,8 @@
+package seminar_2;
+
+public interface ActorBehavior {
+    void setMakeOrder();
+    void setTakeOrder();
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+}
